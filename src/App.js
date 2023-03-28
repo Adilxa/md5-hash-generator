c316783fbdd97ee23959cb94aeefd794ab494630
Adilxa <adilhanimerov82@gmail.com>
2023-03-28 14:45:09 +0600

    md5

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import CryptoJS from "crypto-js";
+import { useState } from "react";
+import "./App.scss";
 
-function App() {
+const App = () => {
+
+  const [data, setData] = useState("");
+  
+  const onSave = (data) => {
+    const hash = CryptoJS.MD5(data).toString();
+    setData(hash);
+  };
+
+  const [text, setText] = useState("");
+  function handleFileUpload(event) {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.onload = function (event) {
+      setText(event.target.result);
+    };
+    reader.readAsText(file);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="window">
+        <input type="file" onChange={handleFileUpload} />
+        <h1>md5: {data}</h1>
+        <h1>Text in file: {text}</h1>
+        <button onClick={() => onSave(text)}>Convert to md5</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
